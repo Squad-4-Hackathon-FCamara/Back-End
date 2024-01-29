67ef9013c2a91d0abc999cfe85745fbe1ac83a59
Paulo Ricardo <pr741234@gmail.com>
2024-01-29 15:37:45 -0300

    refactor: Implementado validações de campos no CreateProjectDto
    Implementado validações de campos no CreateProjectDto, para validar as colunas da tabela Project

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,17 +1,60 @@
-import { IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateProjectDto {
-  @IsNotEmpty()
-  @MaxLength(30)
+  @IsString({
+    message: 'O título deve ser uma string',
+  })
+  @IsNotEmpty({
+    message: 'O título deve ser preenchido',
+  })
+  @MaxLength(30, {
+    message: 'O título deve ter no máximo 30 caracteres',
+  })
   title: string;
 
-  @IsNotEmpty()
+  @IsString({
+    message: 'A URL deve ser uma string',
+  })
+  @IsNotEmpty({
+    message: 'A URL deve ser preenchida',
+  })
+  @IsUrl(
+    {
+      protocols: ['http', 'https', 'ftp'],
+      require_protocol: true,
+    },
+    {
+      message: 'A URL fornecida é inválida.',
+    },
+  )
   url: string;
 
   @IsOptional()
-  @MaxLength(350)
+  @MaxLength(350, {
+    message: 'A descrição deve ter no máximo 350 caracteres',
+  })
   description: string;
 
-  @IsNotEmpty()
+  @IsString({
+    message: 'A URL da thumbnail deve ser uma string',
+  })
+  @IsNotEmpty({
+    message: 'A URL da thumbnail deve ser preenchida',
+  })
+  @IsUrl(
+    {
+      protocols: ['http', 'https', 'ftp'],
+      require_protocol: true,
+    },
+    {
+      message: 'A URL da thumbnail fornecida é inválida.',
+    },
+  )
   thumbnail_url: string;
 }
