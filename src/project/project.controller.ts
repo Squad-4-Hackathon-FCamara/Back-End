700aee036d04ce7615e3d3c75224f58e0435dadf
Gustavo <gustavohdlima@hotmail.com>
2024-01-29 14:48:48 -0300

    feat: Implementando funcionalidade de criar projetos
    Implementando funcionalidade de criar projetos juntamente com o upload da imagem, e alguns ajustes

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -7,11 +7,20 @@ import {
   Param,
   Delete,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  HttpStatus,
+  MaxFileSizeValidator,
+  BadRequestException,
+  Req,
 } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
 
 @Controller('project')
 export class ProjectController {
@@ -19,15 +28,43 @@ export class ProjectController {
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectService.create(createProjectDto);
+  @UseInterceptors(FileInterceptor('file'))
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        fileIsRequired: true,
+        errorHttpStatusCode: HttpStatus.BAD_REQUEST,
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 200000,
+            message: 'Imagem não pode ter mais de 200kb',
+          }),
+          // new FileTypeValidator({
+          //   fileType: '^[a-zA-Z0-9-_.]+.(jpe? g|gif|png)$',
+          // }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Req() req: Request,
+  ) {
+    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png')
+      throw new BadRequestException('Imagem deve ter algum dos formatos: JPG, PNG ou GIF');
+
+    const { id: userId } = req.user as { id: string };
+
+    return this.projectService.create(createProjectDto, userId, file);
   }
 
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
   findByTags() {
     return this.projectService.findByTags();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   findOne(@Param('id') id: string) {
     return this.projectService.findOne(+id);
   }
