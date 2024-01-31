55184de3c44eaa12d5ef602405e7d66f10b04ed1
Giovani <giovani98@proton.me>
2024-01-31 10:56:59 -0300

    chore: Deixa as mensagens de erro mais específicas

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -10,18 +10,18 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   @MaxLength(30, {
-    message: `O primeiro nome deve ter no máximo 30 caracteres`,
+    message: `firstName: O primeiro nome deve ter no máximo 30 caracteres`,
   })
   firstName: string;
 
   @IsString()
   @IsNotEmpty()
   @MaxLength(30, {
-    message: `O último nome deve ter no máximo 30 caracteres`,
+    message: `lastName: O último nome deve ter no máximo 30 caracteres`,
   })
   lastName: string;
 
-  @IsEmail()
+  @IsEmail({}, { message: `email: O email deve ser válido` })
   email: string;
 
   @IsStrongPassword(
@@ -34,7 +34,7 @@ export class CreateUserDto {
     },
     {
       message:
-        'A senha deve conter pelo menos 8 caracteres, sendo eles no mínimo 1 maiúsculo, 1 maiúsculo, 1 símbolo e 1 número.',
+        'password: A senha deve conter pelo menos 8 caracteres, sendo eles no mínimo 1 maiúsculo, 1 maiúsculo, 1 símbolo e 1 número.',
     },
   )
   password: string;
