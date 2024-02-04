6553ee739ea98a024f6bcebf485ded0224c601aa
Gustavo <gustavohdlima@hotmail.com>
2024-02-04 19:47:47 -0300

    refactor: trocando origin por url do front para teste

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.SERVER_PORT);
